6354a53d52db29f797c49d835ac122d797eef2db
ryanbrittes <ryanbrittes027@gmail.com>
2025-12-01 21:09:52 -0400

    [ADD] - Scheme Blink_Blink adicionado.

--- Blink_Blink/blink_blink.ino
+++ Blink_Blink/blink_blink.ino
@@ -1,17 +1,13 @@
 #define LED_PIN 2
 
 void setup() {
-  // Inicializando o pino do LED como saída
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // Acendendo o LED
   digitalWrite(LED_PIN, HIGH);
-  // Esperando 0,5 segundos
   delay(500);
-  // Desligando o LED
+  
   digitalWrite(LED_PIN, LOW);
-  // Esperando mais 0,5 segundos
   delay(500);
-}
+}s
